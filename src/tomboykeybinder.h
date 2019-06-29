0ea7b0d7ef922755ca154788ef2aa5f0fdf338ca
Laurent Napias <tamplan@free.fr>
2019-06-29 11:48:03 +0200

    Remove trailing whitespaces

--- src/tomboykeybinder.h
+++ src/tomboykeybinder.h
@@ -13,7 +13,7 @@
  *
  * You should have received a copy of the GNU Library General Public
  * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110, USA 
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110, USA
  */
 #ifndef __TOMBOY_KEY_BINDER_H__
 #define __TOMBOY_KEY_BINDER_H__

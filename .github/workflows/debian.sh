a76fd08cf382e01c0aa39f103d9bdad2261c0581
mbkma <johannes.unruh@fau.de>
2026-01-27 23:12:48 +0100

    ci: migrate from Travis CI to GitHub Actions
    Replace Travis CI configuration with GitHub Actions workflows.
    Add CI build workflow for Debian, Fedora, Ubuntu, and Archlinux.
    Add release workflow for automated GitHub releases on tags.
    Add dependabot configuration for GHA pin updates.
    Remove obsolete .travis.yml.

---
+++ .github/workflows/debian.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/bash
+
+# Use grouped output messages
+infobegin() {
+	echo "::group::${1}"
+}
+infoend() {
+	echo "::endgroup::"
+}
+
+# Required packages on Debian
+requires=(
+	ccache # Use ccache to speed up build
+)
+
+requires+=(
+	autoconf-archive
+	autopoint
+	gcc
+	git
+	libgtk-3-dev
+	libayatana-ido3-dev
+	libayatana-indicator3-dev
+	libmate-panel-applet-dev
+	libtool
+	libx11-dev
+	libxml2-dev
+	make
+	mate-common
+)
+
+infobegin "Update system"
+apt-get update -qq
+infoend
+
+infobegin "Install dependency packages"
+env DEBIAN_FRONTEND=noninteractive \
+	apt-get install --assume-yes \
+	${requires[@]}
+infoend
---
+++ .github/workflows/fedora.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+
+# Use grouped output messages
+infobegin() {
+	echo "::group::${1}"
+}
+infoend() {
+	echo "::endgroup::"
+}
+
+# Required packages on Fedora
+requires=(
+	ccache # Use ccache to speed up build
+)
+
+requires+=(
+	autoconf-archive
+	gcc
+	git
+	gtk3-devel
+	libindicator-gtk3-devel
+	make
+	mate-common
+	mate-panel-devel
+	redhat-rpm-config
+)
+
+infobegin "Update system"
+dnf update -y
+infoend
+
+infobegin "Install dependency packages"
+dnf install -y ${requires[@]}
+infoend
